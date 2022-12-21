167ea3851dc25b5cb8564835e898d898e3e4db72
Bruno Lima <contato@blimas.dev>
2022-12-20 23:42:24 -0300

    chore: cria projeto inicial

---
+++ src/main/java/com/padawanbr/alfredfood/AlfredfoodApplication.java
@@ -0,0 +1,13 @@
+package com.padawanbr.alfredfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlfredfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlfredfoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/padawanbr/alfredfood/AlfredfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.padawanbr.alfredfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlfredfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
