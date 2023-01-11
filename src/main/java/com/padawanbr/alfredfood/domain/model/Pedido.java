e4ac90ac7a3b1049097ae6236f63c2075477442d
Bruno Lima <contato@blimas.dev>
2023-01-10 23:46:31 -0300

    criando pool de conexões e implementando Flyway

---
+++ src/main/java/com/padawanbr/alfredfood/domain/model/Pedido.java
@@ -0,0 +1,63 @@
+package com.padawanbr.alfredfood.domain.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Pedido {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal subtotal;
+    private BigDecimal taxaFrete;
+    private BigDecimal valorTotal;
+
+    @Embedded
+    private Endereco enderecoEntrega;
+
+    private StatusPedido status;
+
+    @CreationTimestamp
+    @Column(nullable = false, columnDefinition = "datetime")
+    private LocalDateTime dataCadastro;
+
+    @Column( columnDefinition = "datetime")
+    private LocalDateTime dataConfirmacao;
+
+    @Column(columnDefinition = "datetime")
+    private LocalDateTime dataCancelamento;
+
+    @UpdateTimestamp
+    @Column(columnDefinition = "datetime")
+    private LocalDateTime dataEntrega;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private FormaPagamento formaPagamento;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Restaurante restaurante;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_cliente_id", nullable = false)
+    private Usuario cliente;
+
+    @OneToMany(mappedBy = "pedido")
+    private List<ItemPedido> itens = new ArrayList<>();
+
+}
---
+++ src/main/resources/db/migration/V3.0.0__init-table-estado.sql
@@ -0,0 +1,18 @@
+
+CREATE TABLE estado (
+                        id bigint not null auto_increment,
+                        nome varchar(80) not null,
+
+                        primary key (id)
+) engine=InnoDB default charset=utf8;
+
+INSERT INTO estado (nome) SELECT DISTINCT nome_estado FROM alfredfood.cidade;
+ALTER TABLE cidade ADD COLUMN estado_id bigint not null;
+
+UPDATE cidade c SET c.estado_id = (SELECT e.id FROM estado e WHERE e.nome = c.nome_estado);
+
+alter table cidade add constraint fk_cidade_estado
+    foreign key (estado_id) references estado(id);
+
+alter table cidade drop column nome_estado;
+alter table cidade change nome_cidade nome varchar(80) not null;
