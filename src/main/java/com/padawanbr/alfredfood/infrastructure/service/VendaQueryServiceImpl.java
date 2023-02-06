c8ec69567ca6fe05e2e8adc508e8b01642c13d60
Bruno Lima <contato@blimas.dev>
2023-02-05 22:39:05 -0300

    adicionando os filtros na consulta de vendas diárias

--- src/main/java/com/padawanbr/alfredfood/infrastructure/service/VendaQueryServiceImpl.java
+++ src/main/java/com/padawanbr/alfredfood/infrastructure/service/VendaQueryServiceImpl.java
@@ -2,6 +2,7 @@ package com.padawanbr.alfredfood.infrastructure.service;
 
 import com.padawanbr.alfredfood.domain.filter.VendaDiariaFilter;
 import com.padawanbr.alfredfood.domain.model.Pedido;
+import com.padawanbr.alfredfood.domain.model.StatusPedido;
 import com.padawanbr.alfredfood.domain.model.dto.VendaDiaria;
 import com.padawanbr.alfredfood.domain.service.VendaQueryService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.criteria.*;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -36,7 +38,23 @@ public class VendaQueryServiceImpl implements VendaQueryService {
                 criteriaBuilder.count(root.get("id")),
                 criteriaBuilder.sum(root.get("valorTotal")));
 
+        var predicates = new ArrayList<>();
+
+        if (vendaDiariaFilter.getRestauranteId() != null) {
+            predicates.add(criteriaBuilder.equal(root.get("restaurante"), vendaDiariaFilter.getRestauranteId()));
+        }
+        if (vendaDiariaFilter.getDataCriacaoInicio() != null) {
+            predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get("dataCriacao"), vendaDiariaFilter.getRestauranteId()));
+        }
+        if (vendaDiariaFilter.getDataCriacaoFim() != null) {
+            predicates.add(criteriaBuilder.lessThanOrEqualTo(root.get("dataCriacao"), vendaDiariaFilter.getRestauranteId()));
+        }
+
+        predicates.add(root.get("status").in(
+                StatusPedido.CONFIRMADO, StatusPedido.ENTREGUE));
+
         query.select(vendaDiariaCompoundSelection);
+        query.where(predicates.toArray(new Predicate[0]));
         query.groupBy(localDateExpression);
 
         return entityManager.createQuery(query).getResultList();
